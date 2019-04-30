8324d1c6a6f4fa103e9af6e86a37ebfaabe64d67
saul205 <45789194+saul205@users.noreply.github.com>
2019-04-30 21:36:41 +0200

    Add files via upload

--- practica_4.sh
+++ practica_4.sh
@@ -4,31 +4,36 @@
 
 leerBorrar(){
     oldIFS=$IFS
-    IFS=','
-    mkdir -p /extra/backup
+    IFS=,
 	while read ip
 	do
-		ping -c 1 $ip > /dev/null 
+		echo "$ip"
+		ping -c 1 $ip > /dev/null
 		if [ $? -ne 0 ]
 		then
 			echo "$ip no es accesible"
 		else
+			ssh -n user@$ip "sudo mkdir -p /extra/backup"
 			while read nombre ignore
 			do
-				ssh "as@$ip id $nombre 2> /dev/null > /dev/null"
+				ssh -n user@$ip "id $nombre 2> /dev/null > /dev/null"
 				if [ $? -eq 0 ]
 				then
-					uhome=$(ssh as@$ip "cat /etc/passwd | egrep "$nombre" | grep -o ":/[^:]*:" | tr -d ':'")
-					ssh as@$ip "tar -cf /extra/backup/$nombre.tar $uhome 2> /dev/null > /dev/null"
+					echo $nombre
+					uhome=$(ssh -n user@$ip "cat /etc/passwd | grep "${nombre}:" | cut -d ':' -f 6")
+					echo "soy $uhome"
+					echo adios
+					ssh -n user@$ip "sudo tar -cvf /extra/backup/${nombre}.tar $uhome 2> /dev/null > /dev/null"
 					if [ $? -eq 0 ]
 					then
-						ssh as@$ip "userdel -r $nombre 2> /dev/null"
+						echo "Eliminando"
+						ssh -n user@$ip "sudo userdel -r $nombre 2> /dev/null"
 					fi
 				fi
 			done < $1
-		fi	
+		fi
 	done < $2
-    IFS=$oldIFS      
+    IFS=$oldIFS
 }
 
 leerInsertar(){
@@ -36,6 +41,7 @@ leerInsertar(){
     IFS=,
 	while read ip
 	do
+		echo $ip
 		ping -c 1 $ip > /dev/null 
 		if [ $? -ne 0 ]
 		then
@@ -43,28 +49,30 @@ leerInsertar(){
 		else
 			while read nombre contrasena nomC
 			do
+				echo "$nombre" "$contrasena" $nomC
 				if [[ -z "$nombre" || -z "$contrasena" || -z "${nomC}" ]]
 				then
 					echo "Campo invalido"
 				else
-					ssh as@$ip "useradd -c "$nomC" $nombre -m -k /etc/skel -K UID_MIN=1815 -U 2> /dev/null"
+					ssh -n  user@$ip "sudo useradd -c "$nomC" $nombre -m -k /etc/skel -K UID_MIN=1815 -U 2> /dev/null"
 					anyadido=$?
+					echo $anyadido
 					if [ $anyadido -eq 9 ]
 					then
 							echo "El usuario $nombre ya existe"
 					elif [ $anyadido -eq 0 ]
 					then
-						ssh as@$ip "usermod -f0 $nombre"
-						ssh as@$ip "echo "${nombre}:$contrasena" | chpasswd"
-						ssh as@$ip "passwd -x30 $nombre > /dev/null"
-			 
+						echo añadido
+						ssh -n user@$ip "sudo usermod -f0 $nombre"
+						ssh -n user@$ip "echo "${nombre}:$contrasena" | sudo chpasswd"
+						ssh -n user@$ip "sudo passwd -x30 $nombre > /dev/null"
 						echo "$nomC ha sido creado"
 					fi
 				fi
 			done < $1
 		fi
-	done < $2	
-    IFS=$oldIFS      
+	done < $2
+    IFS=$oldIFS
 }
 
 
@@ -77,67 +85,10 @@ then
     else
         if [ $1 == "-a" ]
         then
-            oldIFS=$IFS
-    IFS=,
-	while read ip
-	do
-		ping -c 1 $ip > /dev/null 
-		if [ $? -ne 0 ]
-		then
-			echo "$ip no es accesible"
-		else
-			while read nombre contrasena nomC
-			do
-				if [[ -z "$nombre" || -z "$contrasena" || -z "${nomC}" ]]
-				then
-					echo "Campo invalido"
-				else
-					ssh as@$ip "sudo useradd -c "$nomC" $nombre -m -k /etc/skel -K UID_MIN=1815 -U 2> /dev/null"
-					anyadido=$?
-					if [ $anyadido -eq 9 ]
-					then
-							echo "El usuario $nombre ya existe"
-					elif [ $anyadido -eq 0 ]
-					then
-						ssh as@$ip "sudo usermod -f0 $nombre"
-						ssh as@$ip "echo "${nombre}:$contrasena" | sudo chpasswd"
-						ssh as@$ip "sudo passwd -x30 $nombre > /dev/null"
-			 
-						echo "$nomC ha sido creado"
-					fi
-				fi
-			done < $2
-		fi
-	done < $3	
-    IFS=$oldIFS      
+            leerInsertar $2 $3
         elif [ $1 == "-s" ]
         then
-            oldIFS=$IFS
-    IFS=','
-    mkdir -p /extra/backup
-	while read ip
-	do
-		ping -c 1 $ip > /dev/null 
-		if [ $? -ne 0 ]
-		then
-			echo "$ip no es accesible"
-		else
-			while read nombre ignore
-			do
-				ssh "as@$ip id $nombre 2> /dev/null > /dev/null"
-				if [ $? -eq 0 ]
-				then
-					uhome=$(ssh as@$ip "cat /etc/passwd | egrep "$nombre" | grep -o ":/[^:]*:" | tr -d ':'")
-					ssh as@$ip "tar -cf /extra/backup/$nombre.tar $uhome 2> /dev/null > /dev/null"
-					if [ $? -eq 0 ]
-					then
-						ssh as@$ip "sudo userdel -r $nombre 2> /dev/null"
-					fi
-				fi
-			done < $2
-		fi	
-	done < $3
-    IFS=$oldIFS  
+            leerBorrar $2 $3
         else
             echo "Opcion invalida" >&2
         fi
