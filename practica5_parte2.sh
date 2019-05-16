eed8a9fed7c92f4e995f2e1d4ac49d6260d64a79
Luis Garcia <45603226+luisgg98@users.noreply.github.com>
2019-05-16 19:54:48 +0200

    Add files via upload

--- practica5_parte2.sh
+++ practica5_parte2.sh
@@ -5,16 +5,15 @@ then
     echo "Numero de parametros incorrecto"
     exit 85
 fi
-ip=$1
-if ! ping -c1 "$ip" > /dev/null
+ip=$(echo "$1" | cut -d '@' -f2)
+
+if ! ping -c1 "$ip"  > /dev/null
 then
-    echo "No se puede acceder a $ip"
+    echo "No se puede acceder a la direccion: $ip"
     exit 1
 fi
 
-ssh as@ip sudo sfdisk -s
-ssh as@ip sudo sfdisk -l
-ssh as@ip sudo df -hT
-
-echo "Fin del script"
+ssh "$1" "sudo sfdisk -s"
+ssh "$1" "sudo sfdisk -l"
+ssh "$1" "sudo df -hT"
 
