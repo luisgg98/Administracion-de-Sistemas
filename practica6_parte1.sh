ae54446d49c647583da3042d2ec9762456d4c7ee
Luis Garcia <45603226+luisgg98@users.noreply.github.com>
2019-05-31 10:48:55 +0200

    Add files via upload

--- practica6_parte1.sh
+++ practica6_parte1.sh
@@ -2,13 +2,6 @@
 
 #Autores: Saúl Flores Benavente 755769
 #		  Luis García Garcés 739202
-
-if [ $# -ne 1 ]; then #Si el numero de paramtros es incorrecto
-	echo "Numero de parametros incorrecto"
-	exit 1
-else
-ip=$1
-#El script va monitorizar la información tanto de la máquina local como de la remota 
 #Empezamos monitorizando la máquina local
 	logger -p local0.info "Informacion Maquina local:"
 
@@ -26,22 +19,3 @@ ip=$1
 
 	logger -p local0.info "Numero de programas en ejecucion:"
 	 ps | logger -p local0.info
-
-#Ahora se guarda la monitorizacion de la maquina remota
-	logger -p local0.info "Informacion Maquina remota "${ip}""
-
-	logger -p local0.info "Numero de usuarios y carga media de trabajo:"
-	ssh "${ip}" "uptime" | logger -p local0.info
-
-	logger -p local0.info "Memoria ocupada y libre, y swap usado:"
-	ssh "${ip}" " free" | logger -p local0.info
-
-	logger -p local0.info "Espacio ocupado y libre:"
-	ssh "${ip}" " df" | logger -p local0.info
-
-	logger -p local0.info "Puertos abiertos y conexiones establecidas:"
-	ssh "${ip}" " netstat" | logger -p local0.info
-
-	logger -p local0.info "Numero de programas en ejecucion:"
-	ssh "${ip}" " ps" | logger -p local0.info
-fi
